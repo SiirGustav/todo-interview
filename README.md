f93438178484b66b84418403d8c306ee457858cb
SiirGustav <sirgustav@outlook.com>
2023-03-13 09:33:33 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,27 +1,28 @@
-# TodoInterview
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://www.allrideapp.com/">
+    <img src="https://www.allrideapp.com/assets/img/allride_celeste.svg" alt="Logo" width="180">
+  </a>
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.8.
+  <h2 align="center">Quick Start Prueba Técnica - Frontend</h2>
 
-## Development server
+</div>
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+Dado que tanto el conocimiento de Angular 8, como websockets, es opcional para el puesto de frontend ofrecido, creamos esta plantilla de inicio rápido, en la cual se podrán encontrar las siguientes funcionalidades:
 
-## Code scaffolding
+* Conexiones al websocket
+* Manejo de eventos recibidos del socket
+* Métodos para emitir ciertos eventos al socket
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+Es importante mencionar que, para la correcta conexión a tu room personalizada, debes cambiar el string
+`'tu_room_personal'`, ubicado en `src\services\web-socket-service.ts`, por tu usuario de Github. Luego de eso, sólo necesitarás crear el diseño de la página y hacer uso de (y modificar de ser necesario) los métodos existentes.
 
-## Build
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+## Detalles del proyecto
 
-## Running unit tests
+Generado con [Angular CLI](https://github.com/angular/angular-cli) version 8.3.8.
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+## Servidor de desarrollo
 
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+Correr `ng serve` en consola para iniciar el servidor. Navegar a `http://localhost:4200/`. La aplicación recargará la página automáticamente ante cualquier cambio guardado.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WebSocketService } from 'src/services/web-socket-service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,139 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-interview';
+  constructor(private webSocketService: WebSocketService){
+    
+  }
+
+  async ngOnInit(){
+    this.webSocketService.connect();
+		this.webSocketService.emit('join');
+
+    console.log(this.webSocketService);
+
+    this.webSocketService.callback.subscribe(res => {
+      console.log("update", res);
+    });
+
+    this.webSocketService.on('connect', (error, callback) => {
+			console.log('\x1b[36m%s\x1b[0m', "=========== Connected succesfully ============");
+      console.log(this);
+		});
+
+    this.webSocketService.on('reconnect', (error, callback) => {
+      console.log('\x1b[36m%s\x1b[0m', "=========== Reconnected succesfully ============");
+      console.log(this);
+			this.webSocketService.connect();
+			this.webSocketService.emit('join');
+		});
+
+		this.webSocketService.on('connect_error', (error, callback) => {
+      console.log('\x1b[36m%s\x1b[0m', "=========== Error with socket ============");
+      console.log(error);
+		});
+
+    this.handleSocketConnections();
+  }
+
+  emitComplete(id: string){
+    /* Receives id as string, you can edit this if necessary */
+    this.webSocketService.emit('completeTask', id);
+  }
+
+  emitDelete(id: string){
+    /* Receives id as string, you can edit this if necessary */
+    this.webSocketService.emit('deleteTask', id);
+  }
+
+  emitCreate(){
+    let newTask = {
+      /* Your task schema */
+    }
+
+    this.webSocketService.emit('createTask', newTask);
+  }
+
+  emitEdit(id: string){
+    /* Receives id as string, you can edit this if necessary */
+    let data = {
+      id: id,
+      editedTask: {
+        /* Your task schema */
+      }
+    }
+
+    this.webSocketService.emit('editTask', data);
+  }
+
+  getCurrentClients(){
+    this.webSocketService.emit('getClients');
+  }
+
+  /* == Socket == */
+  handleSocketConnections() {
+    /* Subscribes to all types of socket events, in order to print them on console. You can edit this if necessary. */
+    this.newClient().subscribe(data => {
+			console.log('\x1b[36m%s\x1b[0m', "============= New Client ==========");
+      console.log(data);
+		});
+
+
+    this.disconnectedClient().subscribe(data => {
+			console.log('\x1b[36m%s\x1b[0m', "============= Disconnected Client ==========");
+      console.log(data);
+		});
+
+    this.getClients().subscribe(data => {
+			console.log('\x1b[36m%s\x1b[0m', "============= Clients ==========");
+      console.log(data);
+		});
+
+    this.completeTask().subscribe(data => {
+			console.log('\x1b[36m%s\x1b[0m', "============= Complete Task ==========");
+      console.log(data);
+		});
+
+    this.deleteTask().subscribe(data => {
+			console.log('\x1b[36m%s\x1b[0m', "============= Delete Task ==========");
+      console.log(data);
+		});
+
+    this.createTask().subscribe(data => {
+			console.log('\x1b[36m%s\x1b[0m', "============= Create Task ==========");
+      console.log(data);
+		});
+
+    this.editTask().subscribe(data => {
+			console.log('\x1b[36m%s\x1b[0m', "============= Edit Task ==========");
+      console.log(data);
+		});
+  }
+
+  newClient() {
+		return this.webSocketService.fromEvent<any>('newClient');
+	}
+
+  disconnectedClient() {
+		return this.webSocketService.fromEvent<any>('disconnectedClient');
+	}
+
+  getClients() {
+		return this.webSocketService.fromEvent<any>('getClients');
+	}
+
+  completeTask() {
+		return this.webSocketService.fromEvent<any>('completeTask');
+	}
+
+  deleteTask() {
+		return this.webSocketService.fromEvent<any>('deleteTask');
+	}
+
+  createTask() {
+		return this.webSocketService.fromEvent<any>('createTask');
+	}
+
+  editTask() {
+		return this.webSocketService.fromEvent<any>('editTask');
+	}
 }
